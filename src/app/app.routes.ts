41f2ff66e61c43577fb4c0fdf3bff9f24bd1df2d
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-07 21:39:53 +0530

    basic template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'about', component: AboutComponent },
+];

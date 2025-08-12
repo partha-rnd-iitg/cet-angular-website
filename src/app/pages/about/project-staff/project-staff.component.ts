fdde5ab6c6266575e3708c92429cd014bfa148a9
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-12 14:41:04 +0530

    fix/link to homepage from OfficeStaff and ProjectStafff page

--- src/app/pages/about/project-staff/project-staff.component.ts
+++ src/app/pages/about/project-staff/project-staff.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-staff',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './project-staff.component.html',
   styleUrl: './project-staff.component.css'
 })

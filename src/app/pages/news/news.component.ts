a7716e89106a7f3fb11c855a3a3b65c12469d747
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-15 12:04:27 +0530

    fix/news-section takes data from external .json

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NEWS_ARTICLES, NewsArticle } from "./news-data"
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
 })
 export class NewsComponent {
-
+  newsArtcileArray : NewsArticle[] = NEWS_ARTICLES;
 }

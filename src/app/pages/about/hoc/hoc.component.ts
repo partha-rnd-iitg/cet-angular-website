acdb4c13af9ae624ec6c82eb2b5abab1f4058b0f
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-15 15:26:25 +0530

    fix/principal-investigators page

--- src/app/pages/about/hoc/hoc.component.ts
+++ src/app/pages/about/hoc/hoc.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hoc',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hoc.component.html',
   styleUrl: './hoc.component.css'
 })
--- src/app/pages/about/principal-investigators/principal-investigators.component.ts
+++ src/app/pages/about/principal-investigators/principal-investigators.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-principal-investigators',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './principal-investigators.component.html',
   styleUrl: './principal-investigators.component.css'
 })

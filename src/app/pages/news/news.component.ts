095c3a4549a7750911ea8db7d19f438c58bdfcbc
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-15 10:09:47 +0530

    news template is ready

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
 })

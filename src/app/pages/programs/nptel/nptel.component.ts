05ec410cf97cb0e2d1f3688f83c7866f92cdb7f2
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-08-18 12:39:11 +0530

    Fixed logo and some content

--- src/app/pages/programs/nptel/nptel.component.ts
+++ src/app/pages/programs/nptel/nptel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nptel',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nptel.component.html',
   styleUrl: './nptel.component.css'
 })
